374d0a5c5d8dcaf39b156909b04b8bd6b20f3681
scott <spayton.c@gmail.com>
2020-01-13 09:06:58 -0700

    Fix export in olympians route file

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
 const router= express.Router();
-const database = require('../../../config')
+const database = require('../../../config');
 const olympian = require('../../../models/olympian_pojo');
 
 
 router.get('/', (request, response)=>{
-olympian.getOlympians()
-    .then(
-      data => {
-        if (data.length) {
-          response.status(200).send(data)
-        } else {
-          response.status(200).send({message: "No olympians seeded in db."})
+  olympian.getOlympians()
+      .then(
+        data => {
+          if (data.length) {
+            response.status(200).send(data)
+          } else {
+            response.status(200).send({message: "No olympians seeded in db."})
+          }
         }
-      }
-    ).catch(error => response.status(500).send(error));
-});
+      ).catch(error => response.status(500).send(error));
+  });
 module.exports = router;
