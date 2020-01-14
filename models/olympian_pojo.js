c2782d04f1df1b174367b12bae9be52a8e11bdc0
scott <spayton.c@gmail.com>
2020-01-14 11:10:00 -0700

    Add get female, male, distince olympian and average age methods for pojo

--- models/olympian_pojo.js
+++ models/olympian_pojo.js
@@ -6,21 +6,46 @@ class Olympian{
   }
  static getOlympians() {
     return database('olympians').columns(['name', 'sex', 'age', 'height', 'weight', 'team', 'games', 'sport', 'event', 'medal'])
-  }
+  }//end get olympians
 
  static getYoungest(){
    return database('olympians').min('age').then(function(data){
     return database('olympians').columns(['name', 'sex', 'age', 'team', 'sport', 'medal'])
      .where('age', data[0].min);
    })
- }
+ }//end get youngest
 
  static getOldest(){
    return database('olympians').max('age').then(function(data){
     return database('olympians').columns(['name', 'sex', 'age', 'team', 'sport', 'medal'])
      .where('age', data[0].max);
    })
- }
+ }//end get oldest
 
+ // -----------------------------------
+ // DETAILS FOR OLYMPIAN getSTATS()
+ //------------------------------------
+allOlympiansUnique(){
+  //get all unique olympians
+  return database('olympians').distinct('name').select('name');
+ }
+femaleOlympians(){
+  //get female avg weight
+  return database('olympians').where('sex', 'F').avg('weight');
 }
+maleOlympians(){
+  //get male avg weight
+  return database('olympians').where('sex', 'M').avg('weight');
+}
+averageAge(){
+  return database('olympians').avg('age');
+}
+// -----------------------------------
+// Generate return obj
+//------------------------------------
+
+ static getStats(){
+   
+ }
+}//end class
 module.exports = Olympian
